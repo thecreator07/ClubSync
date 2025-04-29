debd5bbcc1eec1fa8294b2e9794c0886555eff79
Aman Kumar Prasad <amankumarprasad72@gmail.com>
2025-04-29 12:48:33 +0530

    deploy

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,13 @@ export default function Home() {
   return (
     <>
      <Navbar/>
-    ram
+      <div className="container mx-auto p-6">
+        <h1 className="text-3xl font-bold text-center">Welcome to ClubSync</h1>
+        <p className="mt-4 text-center">
+          A platform for managing clubs and events. Explore, create, and join
+          clubs and events that interest you.
+        </p>
+      </div>
     </>
   );
 }
